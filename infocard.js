40d5d318ca71ef76aaad7d4f80c3e7913fc6cdde
welorylimano <54174788+welorylimano@users.noreply.github.com>
2020-01-08 12:41:28 +0500

    Add files via upload

---
+++ infocard.js
@@ -0,0 +1,16 @@
+document.oncontextmenu = function (){return false};
+
+newFunction();
+function newFunction() {
+
+    document.write('<div class="bglogo"><div id="blur"><div class="logo"></div></div></div>');
+    document.write('<div class="inf">')
+    document.write('<div id="productname"><a href="https://aniukki.github.io/"><h1>ANIUKKI</h1></a></div>')
+    document.write('<div id="devinf"><h1>Разработчик:</h1><a href="https://github.com/welorylimano"><p>vvl1m</p></a></div>')
+    document.write('<!--<div id="firstver"><h1>Первая версия:</h1><p><a href="???">???</a></p></div>-->')
+    document.write('<div id="makeby"><h1>Интерфейс: <a href="https://www.w3.org/html/">HTML</a></h1></div>')
+    document.write('<div id="makeby"><h1>Написано на:</h1></div>')
+    document.write('<div id="plang"><img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1280px-PHP-logo.svg.png"></div>')
+    document.write('</div>')
+    document.write('<div id="copyright"><p><a href="https://github.com/TheFunnyDay/infocard.js-addons">Copyright by TheFunnyDay </p></a></div>')
+}
